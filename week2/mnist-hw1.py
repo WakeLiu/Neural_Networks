576c90b5dc4e9f9903be0a4cb01fda89002917d7
Wake Liu <wakeliu@gmail.com>
2017-11-21 19:39:57 +0800

    week2

--- week2/mnist-hw1.py
+++ week2/mnist-hw1.py
@@ -38,7 +38,7 @@ n_labels = y_train.shape[1] # 10
 with tf.Session(config=tf.ConfigProto()) as sess:
     with tf.name_scope('inputs'):
         x = tf.placeholder(tf.float32, [None, n_features], name = 'digit_input')
-    
+
     with tf.name_scope('labels'):
         y = tf.placeholder(tf.float32, [None, n_labels], name = 'digit_label')
 
@@ -46,40 +46,40 @@ with tf.Session(config=tf.ConfigProto()) as sess:
     with tf.name_scope('params'):
         w = tf.Variable(tf.zeros([n_features, n_labels]), name = 'weight')
         b = tf.Variable(tf.zeros([n_labels]), name = 'bias')
-    
+
     # build model
     with tf.name_scope('model'):
         prediction = tf.nn.softmax(tf.matmul(x, w) + b)
-    
+
     # define loss
     with tf.name_scope('loss'):
         loss = tf.reduce_mean(-tf.reduce_sum(y * tf.log(prediction), reduction_indices = 1))
         tf.summary.scalar('Loss', loss)
-    
+
     # Gradient Descent
     with tf.name_scope('gd'):
         optimizer = tf.train.GradientDescentOptimizer(lr).minimize(loss)
-    
+
     with tf.name_scope('accuracy'):
         correct_prediction = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
         acc = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
         tf.summary.scalar('Accuracy', acc)
-    
+
     # start run
     initializer = tf.global_variables_initializer()
     sess.run(initializer)
-    
+
     # display data
     merged = tf.summary.merge_all()
     writer = tf.summary.FileWriter(logs_path, graph = sess.graph)
-    
-    for step in xrange(train_steps):
+
+    for step in range(train_steps):
         x_batch, y_batch = mnist.train.next_batch(batch_size)
         sess.run(optimizer, feed_dict = {x: x_batch, y: y_batch})
-    
+
         if step % 50 == 0:
             l, summary = sess.run([loss, merged], feed_dict = {x: x_batch, y: y_batch})
             ac = sess.run(acc, feed_dict={x: x_test, y: y_test})
             writer.add_summary(summary, step)
-            
+
             print("Accuracy: %.2f, Loss: %.2f" % (ac, l))
